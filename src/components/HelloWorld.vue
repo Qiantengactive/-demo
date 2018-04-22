def545109a8cb9f42b777a51ebfa28504e4c8054
qt-laptop <936129056@qq.com>
2018-04-22 23:22:52 +0800

    first

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <div class="amap-wrapper">
+      <el-amap class="amap-box" :vid="'amap-vue'"></el-amap>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'vue-amap向你问好'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.amap-wrapper {
+  width: 100vw;
+  height: 500px;
+  margin: auto;
+}
+</style>
